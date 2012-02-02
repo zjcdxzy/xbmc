12eb65b089d1c763ea240f1b3d64861253160c7a
Daniela Engert <dani@ngrt.de>
2012-02-02 18:14:05 +0100

    add codec profile information for audio streams

--- xbmc/cores/dvdplayer/DVDStreamInfo.cpp
+++ xbmc/cores/dvdplayer/DVDStreamInfo.cpp
@@ -183,6 +183,7 @@ void CDVDStreamInfo::Assign(const CDemuxStream& right, bool withextradata)
     blockalign    = stream->iBlockAlign;
     bitrate       = stream->iBitRate;
     bitspersample = stream->iBitsPerSample;
+    profile       = stream->profile;
   }
   else if(  right.type == STREAM_VIDEO )
   {
