8ba2c38c728332e2faf81236772d3bc5c8a31845
Memphiz <memphis@machzwo.de>
2014-02-23 23:40:08 +0100

    [ios] - always send a speed change to ios when the player gets paused (to stop the playback progress bar in the ios control center)

--- xbmc/osx/ios/XBMCController.mm
+++ xbmc/osx/ios/XBMCController.mm
@@ -44,6 +44,7 @@
 #include "utils/Variant.h"
 #include "Util.h"
 #include "threads/Event.h"
+#include "osx/DarwinUtils.h"
 #define id _id
 #include "TextureCache.h"
 #undef id
@@ -180,7 +181,7 @@ void AnnounceBridge(ANNOUNCEMENT::AnnouncementFlag flag, const char *sender, con
     LOG(@"item: %@", item.description);
     [g_xbmcController performSelectorOnMainThread:@selector(onPlay:) withObject:item  waitUntilDone:NO];
   }
-  else if (msg == "OnSpeedChanged")
+  else if (msg == "OnSpeedChanged" || msg == "OnPause")
   {
     NSDictionary *item = [dict valueForKey:@"item"];
     NSDictionary *player = [dict valueForKey:@"player"];
@@ -188,10 +189,8 @@ void AnnounceBridge(ANNOUNCEMENT::AnnouncementFlag flag, const char *sender, con
     [item setValue:[NSNumber numberWithDouble:g_application.GetTime()] forKey:@"elapsed"];
     LOG(@"item: %@", item.description);
     [g_xbmcController performSelectorOnMainThread:@selector(OnSpeedChanged:) withObject:item  waitUntilDone:NO];
-  }
-  else if (msg == "OnPause")
-  {
-    [g_xbmcController performSelectorOnMainThread:@selector(onPause:) withObject:[dict valueForKey:@"item"]  waitUntilDone:NO];
+    if (msg == "OnPause")
+      [g_xbmcController performSelectorOnMainThread:@selector(onPause:) withObject:[dict valueForKey:@"item"]  waitUntilDone:NO];
   }
   else if (msg == "OnStop")
   {
