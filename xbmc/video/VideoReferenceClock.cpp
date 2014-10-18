db963725b85eb9f6bc712884678c2b1bd976f5a2
Rainer Hochecker <fernetmenta@online.de>
2014-10-18 11:47:02 +0200

    videorefclock: add video sync drm

--- xbmc/video/VideoReferenceClock.cpp
+++ xbmc/video/VideoReferenceClock.cpp
@@ -28,9 +28,13 @@
 #include "threads/SingleLock.h"
 #include "guilib/GraphicContext.h"
 #include "video/videosync/VideoSync.h"
+#include "windowing/WindowingFactory.h"
 
 #if defined(HAS_GLX)
 #include "video/videosync/VideoSyncGLX.h"
+#endif
+#if defined(HAVE_X11)
+#include "video/videosync/VideoSyncDRM.h"
 #endif
 #if defined(TARGET_WINDOWS)
 #include "video/videosync/VideoSyncD3D.h"
@@ -88,7 +92,12 @@ void CVideoReferenceClock::Process()
   while(!m_bStop)
   {
     //set up the vblank clock
-#if defined(HAS_GLX)
+#if defined(HAVE_X11)
+  std::string gpuvendor = g_Windowing.GetRenderVendor();
+  std::transform(gpuvendor.begin(), gpuvendor.end(), gpuvendor.begin(), ::tolower);
+  if (gpuvendor.compare(0, 5, "intel") == 0)
+    m_pVideoSync = new CVideoSyncDRM();
+  else
     m_pVideoSync = new CVideoSyncGLX();
 #elif defined(TARGET_WINDOWS)
     m_pVideoSync = new CVideoSyncD3D();
