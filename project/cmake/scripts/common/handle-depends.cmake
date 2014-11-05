fafdc94f22b8efee65ea155a4663a14769fa9429
montellese <montellese@xbmc.org>
2014-11-05 09:55:13 +0100

    binary addons: don't do any addon dependency processing if a matching cmake build target already exists

--- project/cmake/scripts/common/handle-depends.cmake
+++ project/cmake/scripts/common/handle-depends.cmake
@@ -33,80 +33,80 @@ function(add_addon_depends addon searchpath)
         get_filename_component(id ${file} NAME_WE)
       endif()
 
-      # check if there are any library specific flags that need to be passed on
-      if(EXISTS ${dir}/flags.txt)
-        file(STRINGS ${dir}/flags.txt extraflags)
-        separate_arguments(extraflags)
-        message(STATUS "${id} extraflags: ${extraflags}")
-      endif()
-
-      set(BUILD_ARGS -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-                   -DOUTPUT_DIR=${DEPENDS_PATH}
-                   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-                   -DCMAKE_USER_MAKE_RULES_OVERRIDE=${CMAKE_USER_MAKE_RULES_OVERRIDE}
-                   -DCMAKE_USER_MAKE_RULES_OVERRIDE_CXX=${CMAKE_USER_MAKE_RULES_OVERRIDE_CXX}
-                   -DCMAKE_INSTALL_PREFIX=${DEPENDS_PATH}
-                   -DARCH_DEFINES=${ARCH_DEFINES}
-                   -DENABLE_STATIC=1
-                   -DBUILD_SHARED_LIBS=0)
-
-      if(CMAKE_TOOLCHAIN_FILE)
-        list(APPEND BUILD_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE})
-        MESSAGE("toolchain specified")
-        MESSAGE(${BUILD_ARGS})
-      endif()
-
-      # if there's a CMakeLists.txt use it to prepare the build
-      if(EXISTS ${dir}/CMakeLists.txt)
-        file(APPEND ${BUILD_DIR}/${id}/tmp/patch.cmake
-             "file(COPY ${dir}/CMakeLists.txt
-                 DESTINATION ${BUILD_DIR}/${id}/src/${id})\n")
-        set(PATCH_COMMAND ${CMAKE_COMMAND} -P ${BUILD_DIR}/${id}/tmp/patch.cmake)
-      else()
-        set(PATCH_COMMAND "")
-      endif()
-
-      # check if we have patches to apply
-      file(GLOB patches ${dir}/*.patch)
-      list(SORT patches)
-      foreach(patch ${patches})
-        set(PATCH_COMMAND ${CMAKE_COMMAND} -P ${BUILD_DIR}/${id}/tmp/patch.cmake)
-        file(APPEND ${BUILD_DIR}/${id}/tmp/patch.cmake
-             "execute_process(COMMAND patch -p1 -i ${patch})\n")
-      endforeach()
-
-
-      # if there's an install.txt use it to properly install the built files
-      if(EXISTS ${dir}/install.txt)
-        set(INSTALL_COMMAND INSTALL_COMMAND ${CMAKE_COMMAND}
-                                -DINPUTDIR=${BUILD_DIR}/${id}/src/${id}-build/
-                                -DINPUTFILE=${dir}/install.txt
-                                -DDESTDIR=${DEPENDS_PATH}
-                                -DENABLE_STATIC=1
-                                "${extraflags}"
-                                -P ${PROJECT_SOURCE_DIR}/install.cmake)
-      elseif(EXISTS ${dir}/noinstall.txt)
-        set(INSTALL_COMMAND INSTALL_COMMAND "")
-      else()
-        set(INSTALL_COMMAND "")
-      endif()
-
-      # check if there's a deps.txt containing dependencies on other libraries
-      if(EXISTS ${dir}/deps.txt)
-        file(STRINGS ${dir}/deps.txt deps)
-        message(STATUS "${id} Depends: ${deps}")
-      else()
-        set(deps)
-      endif()
-
-      # prepare the setup of the call to externalproject_add()
-      set(EXTERNALPROJECT_SETUP PREFIX build/${id}
-                              CMAKE_ARGS ${extraflags} ${BUILD_ARGS}
-                              PATCH_COMMAND ${PATCH_COMMAND}
-                              ${INSTALL_COMMAND}
-                              ${DEPENDS_COMMAND})
-
       if(NOT TARGET ${id})
+        # check if there are any library specific flags that need to be passed on
+        if(EXISTS ${dir}/flags.txt)
+          file(STRINGS ${dir}/flags.txt extraflags)
+          separate_arguments(extraflags)
+          message(STATUS "${id} extraflags: ${extraflags}")
+        endif()
+
+        set(BUILD_ARGS -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+                     -DOUTPUT_DIR=${DEPENDS_PATH}
+                     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                     -DCMAKE_USER_MAKE_RULES_OVERRIDE=${CMAKE_USER_MAKE_RULES_OVERRIDE}
+                     -DCMAKE_USER_MAKE_RULES_OVERRIDE_CXX=${CMAKE_USER_MAKE_RULES_OVERRIDE_CXX}
+                     -DCMAKE_INSTALL_PREFIX=${DEPENDS_PATH}
+                     -DARCH_DEFINES=${ARCH_DEFINES}
+                     -DENABLE_STATIC=1
+                     -DBUILD_SHARED_LIBS=0)
+
+        if(CMAKE_TOOLCHAIN_FILE)
+          list(APPEND BUILD_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE})
+          MESSAGE("toolchain specified")
+          MESSAGE(${BUILD_ARGS})
+        endif()
+
+        # if there's a CMakeLists.txt use it to prepare the build
+        if(EXISTS ${dir}/CMakeLists.txt)
+          file(APPEND ${BUILD_DIR}/${id}/tmp/patch.cmake
+               "file(COPY ${dir}/CMakeLists.txt
+                   DESTINATION ${BUILD_DIR}/${id}/src/${id})\n")
+          set(PATCH_COMMAND ${CMAKE_COMMAND} -P ${BUILD_DIR}/${id}/tmp/patch.cmake)
+        else()
+          set(PATCH_COMMAND "")
+        endif()
+
+        # check if we have patches to apply
+        file(GLOB patches ${dir}/*.patch)
+        list(SORT patches)
+        foreach(patch ${patches})
+          set(PATCH_COMMAND ${CMAKE_COMMAND} -P ${BUILD_DIR}/${id}/tmp/patch.cmake)
+          file(APPEND ${BUILD_DIR}/${id}/tmp/patch.cmake
+               "execute_process(COMMAND patch -p1 -i ${patch})\n")
+        endforeach()
+
+
+        # if there's an install.txt use it to properly install the built files
+        if(EXISTS ${dir}/install.txt)
+          set(INSTALL_COMMAND INSTALL_COMMAND ${CMAKE_COMMAND}
+                                  -DINPUTDIR=${BUILD_DIR}/${id}/src/${id}-build/
+                                  -DINPUTFILE=${dir}/install.txt
+                                  -DDESTDIR=${DEPENDS_PATH}
+                                  -DENABLE_STATIC=1
+                                  "${extraflags}"
+                                  -P ${PROJECT_SOURCE_DIR}/install.cmake)
+        elseif(EXISTS ${dir}/noinstall.txt)
+          set(INSTALL_COMMAND INSTALL_COMMAND "")
+        else()
+          set(INSTALL_COMMAND "")
+        endif()
+
+        # check if there's a deps.txt containing dependencies on other libraries
+        if(EXISTS ${dir}/deps.txt)
+          file(STRINGS ${dir}/deps.txt deps)
+          message(STATUS "${id} depends: ${deps}")
+        else()
+          set(deps)
+        endif()
+
+        # prepare the setup of the call to externalproject_add()
+        set(EXTERNALPROJECT_SETUP PREFIX build/${id}
+                                CMAKE_ARGS ${extraflags} ${BUILD_ARGS}
+                                PATCH_COMMAND ${PATCH_COMMAND}
+                                ${INSTALL_COMMAND}
+                                ${DEPENDS_COMMAND})
+
         # if there's an url defined we need to pass that to externalproject_add()
         if(DEFINED url AND NOT "${url}" STREQUAL "")
           separate_arguments(EXTERNALPROJECT_SETUP)
@@ -132,9 +132,10 @@ function(add_addon_depends addon searchpath)
         if(deps)
           add_dependencies(${id} ${deps})
         endif()
-        set(${addon}_DEPS ${${addon}_DEPS} ${id})
-        set(${addon}_DEPS "${${addon}_DEPS}" PARENT_SCOPE)
       endif()
+
+      set(${addon}_DEPS ${${addon}_DEPS} ${id})
+      set(${addon}_DEPS "${${addon}_DEPS}" PARENT_SCOPE)
     endif()
   endforeach()
 endfunction()
