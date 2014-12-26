194ea55897d8e832e3e15ee655f0b729d1d62603
Chris "Koying" Browet <cbro@semperpax.com>
2014-12-26 09:47:42 +0100

    fixup c++11

--- xbmc/guilib/GUIBaseContainer.cpp
+++ xbmc/guilib/GUIBaseContainer.cpp
@@ -1227,7 +1227,7 @@ std::string CGUIBaseContainer::GetLabel(int info) const
     break;
   case CONTAINER_CURRENT_ITEM:
     {
-      if (m_items.size() && m_items[0]->IsFileItem() && (boost::static_pointer_cast<CFileItem>(m_items[0]))->IsParentFolder())
+      if (m_items.size() && m_items[0]->IsFileItem() && (std::static_pointer_cast<CFileItem>(m_items[0]))->IsParentFolder())
         label = StringUtils::Format("%i", GetSelectedItem());
       else
         label = StringUtils::Format("%i", GetSelectedItem() + 1);
